f1f4d1c3f72759ceac2238a17e21039b14112e42
Anirudh Kamath <github@kamath.io>
2025-02-05 16:28:20 -0800

    custom-openai

---
+++ stagehand.config.ts
@@ -0,0 +1,79 @@
+import type { ConstructorParams, LogLine } from "@browserbasehq/stagehand";
+import dotenv from "dotenv";
+import { CustomOpenAIClient } from "./custom_client";
+
+dotenv.config();
+
+const StagehandConfig: ConstructorParams = {
+  env:
+    process.env.BROWSERBASE_API_KEY && process.env.BROWSERBASE_PROJECT_ID
+      ? "BROWSERBASE"
+      : "LOCAL",
+  apiKey: process.env.BROWSERBASE_API_KEY /* API key for authentication */,
+  projectId: process.env.BROWSERBASE_PROJECT_ID /* Project identifier */,
+  debugDom: true /* Enable DOM debugging features */,
+  headless: false /* Run browser in headless mode */,
+  logger: (message: LogLine) =>
+    message.message === "creating chat completion"
+      ? console.log(JSON.stringify(message))
+      : console.log(logLineToString(message)) /* Custom logging function */,
+  domSettleTimeoutMs: 30_000 /* Timeout for DOM to settle in milliseconds */,
+  llmClient: new CustomOpenAIClient({
+    modelName: process.env.CUSTOM_OPENAI_MODEL_NAME!,
+    clientOptions: {
+      baseURL: process.env.CUSTOM_OPENAI_BASE_URL!,
+      apiKey: process.env.CUSTOM_OPENAI_API_KEY!,
+    },
+  }),
+};
+export default StagehandConfig;
+
+/**
+ * Custom logging function that you can use to filter logs.
+ *
+ * General pattern here is that `message` will always be unique with no params
+ * Any param you would put in a log is in `auxiliary`.
+ *
+ * For example, an error log looks like this:
+ *
+ * ```
+ * {
+ *   category: "error",
+ *   message: "Some specific error occurred",
+ *   auxiliary: {
+ *     message: { value: "Error message", type: "string" },
+ *     trace: { value: "Error trace", type: "string" }
+ *   }
+ * }
+ * ```
+ *
+ * You can then use `logLineToString` to filter for a specific log pattern like
+ *
+ * ```
+ * if (logLine.message === "Some specific error occurred") {
+ *   console.log(logLineToString(logLine));
+ * }
+ * ```
+ */
+export function logLineToString(logLine: LogLine): string {
+  // If you want more detail, set this to false. However, this will make the logs
+  // more verbose and harder to read.
+  const HIDE_AUXILIARY = true;
+
+  try {
+    const timestamp = logLine.timestamp || new Date().toISOString();
+    if (logLine.auxiliary?.error) {
+      return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message}\n ${logLine.auxiliary.error.value}\n ${logLine.auxiliary.trace.value}`;
+    }
+
+    // If we want to hide auxiliary information, we don't add it to the log
+    return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message} ${
+      logLine.auxiliary && !HIDE_AUXILIARY
+        ? JSON.stringify(logLine.auxiliary)
+        : ""
+    }`;
+  } catch (error) {
+    console.error(`Error logging line:`, error);
+    return "error logging line";
+  }
+}
