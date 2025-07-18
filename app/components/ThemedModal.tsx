1741b1ddadfcf02205144678bec9c524c9669b21
Mattyboy25 <mattodumosu7@outlook.com>
2025-07-18 12:18:28 -0400

    fix: add missing closing div in ThemedModal component

--- app/components/ThemedModal.tsx
+++ app/components/ThemedModal.tsx
@@ -227,6 +227,7 @@ export default function ThemedModal({
                       {subtitle}
                     </p>
                   )}
+                </div>
               </div>
 
               {/* Content */}
